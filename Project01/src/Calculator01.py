0a103153eb2cc76e1547b8ef9c4cf9b0495b61b1
kp731 <78096329+kp731@users.noreply.github.com>
2021-06-24 22:32:48 -0400

    added with square root method

---
+++ Project01/src/Calculator01.py
@@ -0,0 +1,56 @@
+import math
+
+
+def addition(a, b):
+    return a + b
+
+
+def subraction(a, b):
+    return a - b
+
+
+def Multiplication(a, b):
+    return a * b
+
+
+def Division(a, b):
+    return a / b
+
+
+def Square(a, b):
+    return a ** b
+
+
+def SquareRoot(a):
+    return math.sqrt(a)
+
+
+class Calculator:
+    result = 0
+
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subraction(a, b)
+        return self.result
+
+    def Multiplication(self, a, b):
+        self.result = Multiplication(a, b)
+        return self.result
+
+    def Division(self, a, b):
+        self.result = Division(a, b)
+        return self.result
+
+    def Square(self, a, b):
+        self.result = Square(a, b)
+        return self.result
+
+    def SquareRoot(self, a):
+        self.result = SquareRoot(a)
+        return self.result
